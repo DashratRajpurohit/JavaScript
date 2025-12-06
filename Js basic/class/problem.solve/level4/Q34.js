7793cb2a694c45308a002ba154b1f95ad38ecb7f
DashratRajpurohit <dashrat.rajpurohit.cg@gamil.com>
2025-12-06 13:20:37 +0530

    Problem Solving Q32-36

--- Js basic/class/problem.solve/level4/Q34.js
+++ Js basic/class/problem.solve/level4/Q34.js
@@ -1,22 +1,17 @@
-let str ="Javascript"
-let str2=str.toLocaleLowerCase();
+let input = "Hello World";
 
-let obj={
-    a:1,
-    e:1,
-    i:1,
-    o:1,
-    u:1
-}
-
-let count=0;
-
-for(let char of str2){
-    if(obj[char]==1){
-        
-    }
-    else{
-        count++;
+let str = input.toLowerCase();
+let count = 0
+for (let i = 0; i < str.length; i++) {
+    let ascii = str.charCodeAt(i);
+    if (((ascii >= 97 && ascii <= 122))) {
+        if ((ascii == 97) || (ascii == 101) || (ascii == 105) || (ascii == 111) || (ascii == 117)) {
+            continue
+        }
+        else {
+            count++
+        }
     }
 }
-console.log(count);
+
+console.log(count) 
