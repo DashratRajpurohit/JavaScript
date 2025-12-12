0777c54ef8642c2eaeda4ac2bad80a648024fe94
DashratRajpurohit <dashrat.rajpurohit.cg@gamil.com>
2025-12-12 15:45:37 +0530

    P.S Level7 Q61 to Q70

--- Js-basic/class/problem.solve/level7/Q70.js
+++ Js-basic/class/problem.solve/level7/Q70.js
@@ -1,6 +1,10 @@
 let arr = [34,5,25,6,1,7];
-let max = arr[0]
-let Smax = arr[0];
+let max = Infinity
+let Smax = Infinity;
+
+let min1 = Infinity
+let Smin1 = Infinity;
+
 for(let i=0;i<arr.length;i++){
      if(arr[i]<max){
        max = arr[i];
@@ -15,3 +19,14 @@ for(let i=0;i<arr.length;i++){
        }
      }
 console.log("Second small = "+Smax)
+
+for(let i=0;i<arr.length;i++){
+     if(arr[i]<min1){
+       Smin1 = min1;
+       min1 = arr[i];
+     }
+     else if(arr[i]<Smin1 && arr[i]>min1){
+            Smin1=arr[i];
+     }
+    }
+    console.log("Second small = "+Smin1)
