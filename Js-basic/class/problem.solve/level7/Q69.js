777412d3fe2ea6e5a3ca541e61745e9434ce44d4
DashratRajpurohit <dashrat.rajpurohit.cg@gamil.com>
2025-12-12 15:41:35 +0530

    P.S Level7 Q61 to Q70

--- Js-basic/class/problem.solve/level7/Q69.js
+++ Js-basic/class/problem.solve/level7/Q69.js
@@ -1,17 +1,32 @@
 let arr = [1,2,34,5,25,6,7];
-let max = arr[0]
-let Smax = arr[0];
+let max = -Infinity
+let Smax = -Infinity;
+
+
+// for(let i=0;i<arr.length;i++){
+//      if(arr[i]>max){
+//        max = arr[i];
+        
+//      }
+//     }
+
+// for(let i=0;i<arr.length;i++){
+//      if(arr[i]>Smax && arr[i]<max){
+//         Smax =arr[i];
+        
+//        }
+//      }
+
+
 for(let i=0;i<arr.length;i++){
      if(arr[i]>max){
+       Smax = max;
        max = arr[i];
-        
+     }
+     else if(arr[i]>Smax && arr[i]<max){
+            Smax=arr[i];
      }
     }
 
-for(let i=0;i<arr.length;i++){
-     if(arr[i]>Smax && arr[i]<max){
-        Smax =arr[i];
-        
-       }
-     }
+
 console.log("Second Max = "+Smax)
