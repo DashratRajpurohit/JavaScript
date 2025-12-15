67f6eb241f071da1e47af57006a9862bdef2a39c
DashratRajpurohit <dashrat.rajpurohit.cg@gamil.com>
2025-12-15 15:25:58 +0530

    P.S Level8 Q78 madify

--- Js-basic/class/problem.solve/level8/Q78.js
+++ Js-basic/class/problem.solve/level8/Q78.js
@@ -1,7 +1,13 @@
-let arr = [-5,-1,-3,-7,-2]
+let arr = [-5, -1, 3, 7, -2]
 
-let max = -Infinity;
-let min = Infinity;
+let max = Math.max(...arr);
+let min = Math.min(...arr);
+
+console.log(max>0 ? max : null);
+console.log(min<0 ? min : null);
+
+max = -Infinity;
+min = Infinity;
 for(let ch of arr){
     if(ch>max){
         max = ch;
